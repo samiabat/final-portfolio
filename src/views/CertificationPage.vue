6cb238d739278672a803e71cb0aa5cd1669349a4
samiabat <samuelabatneh20@gmail.com>
2023-03-17 01:30:44 +0300

    certificates

--- src/views/CertificationPage.vue
+++ src/views/CertificationPage.vue
@@ -2,31 +2,6 @@
     <v-app id="home">
         <Navbar />
         <v-container fluid>
-            <div class="head">
-                <v-row>
-                    <v-col cols="5">
-                        <div style="position: relative;" class="mt-16">
-                            <h1 class="text-grey">HELLO, </h1>
-                            <h1>I'M SAMUEL ABATNEH</h1>
-                            <span class="text-grey">Web Scrapper, AI & Software Engineer</span>
-                            <br>
-                            <v-btn tile dark class="text-yellow mt-8" variant="outlined">Contact me</v-btn>
-                        </div>
-                    </v-col>
-
-                    <v-col cols="2">
-                        <div
-                            style="position: absolute; z-index: 9999; bottom: 0; margin-left: auto; margin-right: auto; left: 0; right: 0; text-align: center;">
-                            <v-icon icon="fas fa-angle-double-down"></v-icon>
-                        </div>
-                    </v-col>
-                    <v-col cols="5">
-                        <div style="position: relative;z-index: 9999;" class="mt-16">
-                            <v-img src="sa.png" contain max-height="300"></v-img>
-                        </div>
-                    </v-col>
-                </v-row>
-            </div>
             <v-col cols="12" class="mt-10" id="about">
                 <v-row class="pa-10">
 
@@ -147,31 +122,9 @@ export default defineComponent({
     text-align: center;
     padding: 12px;
     margin-bottom: 6px;
-    height: 400px;
+    height: 200px;
     width: 100%;
     color: white;
-}
-
-.head::before {
-    content: ' ';
-    position: absolute;
-    top: 0;
-    left: 0;
-    height: 100%;
-    width: 50%;
-    background: black;
-    transform: skew(0deg, 4deg);
-}
-
-.head::after {
-    content: ' ';
-    position: absolute;
-    top: 0;
-    right: 0;
-    height: 100%;
-    width: 50%;
-    background: black;
-    transform: skew(0deg, -4deg);
 }
 
 .egg {
