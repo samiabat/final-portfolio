a8bcda138048e200c82a07d64e74df1217c82f1d
samiabat <samuelabatneh20@gmail.com>
2023-03-19 16:03:32 +0300

    update blog page

--- src/views/BlogPost.vue
+++ src/views/BlogPost.vue
@@ -1,15 +1,56 @@
 <template>
-    <v-app id="home">
-        <!-- <app-navbar/> -->
-      <v-container fluid>
-        <v-row justify="center">
-                <v-col class="text-center">
-                    <v-subheader class="text-h4 justify-center">Blogs</v-subheader>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis illum. </p>
-                </v-col>
-            </v-row>
-            <v-row justify="center">
-                <v-col lg="12" v-for="project in 6" :key="project" class="px-2">
+  <v-app id="home">
+    <!-- <app-navbar/> -->
+    <v-container fluid>
+      <div v-for="project in 6" :key="project">
+        <div class="mx-16 mt-16 px-16 text-center">
+          <h1><u><i>COMPTITIVE PROGRAMMING</i></u> </h1>
+          <h2><u>How I join compititive programming</u></h2>
+        </div>
+        <v-row justify="center" class="mx-16 mt-16">
+          <v-col cols="6" class="pl-16 pr-16">
+            <v-carousel cycle hide-delimiter-background show-arrows="hover">
+              <v-carousel-item src="atrons.png"></v-carousel-item>
+
+              <v-carousel-item src="https://cdn.vuetifyjs.com/images/cards/hotel.jpg"></v-carousel-item>
+
+              <v-carousel-item src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"></v-carousel-item>
+            </v-carousel>
+          </v-col>
+          <v-col cols="6" class="mb-16">
+            <strong>L</strong>orem ipsum dolor sit amet consectetur adipisicing elit. Qui, inventore unde sit molestiae
+            iure recusandae?
+            Necessitatibus praesentium quia saepe consectetur aliquid sit molestiae voluptatum labore! Assumenda quam
+            suscipit quaerat fuga! Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, inventore unde sit
+            molestiae iure recusandae?
+            Necessitatibus praesentium quia saepe consectetur aliquid sit molestiae voluptatum labore! Assumenda quam
+            suscipit quaerat fuga!Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, inventore unde sit
+            molestiae
+            iure recusandae?
+            Necessitatibus praesentium quia saepe consectetur aliquid sit molestiae voluptatum labore! Assumenda quam
+            suscipit quaerat fuga!Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, inventore unde sit
+            molestiae
+            iure recusandae?
+            Necessitatibus praesentium quia saepe consectetur aliquid sit molestiae voluptatum labore! Assumenda quam
+            suscipit quaerat fuga!Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, inventore unde sit
+            molestiae
+            iure recusandae?
+            Necessitatibus praesentium quia saepe consectetur aliquid sit molestiae voluptatum labore! Assumenda quam
+            suscipit quaerat fuga!Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, inventore unde sit
+            molestiae
+            iure recusandae?
+            Necessitatibus praesentium quia saepe consectetur aliquid sit molestiae voluptatum labore! Assumenda quam
+            suscipit quaerat fuga!Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, inventore unde sit
+            molestiae
+            iure recusandae?
+            Necessitatibus praesentium quia saepe consectetur aliquid sit molestiae voluptatum labore! Assumenda quam
+            suscipit quaerat fuga!Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, inventore unde sit
+            molestiae
+            iure recusandae?
+            Necessitatibus praesentium quia saepe consectetur aliquid sit molestiae voluptatum labore! Assumenda quam
+            suscipit quaerat fuga!suscipit quaerat fuga!Lorem ipsum dolor sit amet co.
+          </v-col>
+          <!-- <v-col lg="12" v-for="project in 6" :key="project" class="px-2">
                     <v-card class="mx-auto">
                         <v-img src="atrons.png" cover></v-img>
 
@@ -33,47 +74,74 @@
                         </v-card-actions>
 
                     </v-card>
-                </v-col>
-            </v-row>
-      </v-container>
-    </v-app>
-  </template>
+                
+                  </v-col> -->
+        </v-row>
+        <v-row justify="center" class="mx-16">
+          <v-col cols="12" class="px-16">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, inventore unde sit molestiae iure recusandae?
+            Necessitatibus praesentium quia saepe consectetur aliquid sit molestiae voluptatum labore! Assumenda quam
+            suscipit quaerat fuga!Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, inventore unde sit
+            molestiae
+            iure recusandae?
+            Necessitatibus praesentium quia saepe consectetur aliquid sit molestiae voluptatum labore! Assumenda quam
+            suscipit quaerat fuga!Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, inventore unde sit
+            molestiae
+            iure recusandae?
+            Necessitatibus praesentium quia saepe consectetur aliquid sit molestiae voluptatum labore! Assumenda quam
+            suscipit quaerat fuga!Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, inventore unde sit
+            molestiae
+            iure recusandae?
+            Necessitatibus praesentium quia saepe consectetur aliquid sit molestiae voluptatum labore! Assumenda quam
+            suscipit quaerat fuga!Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, inventore unde sit
+            molestiae
+            iure recusandae?
+            Necessitatibus praesentium quia saepe consectetur aliquid sit molestiae voluptatum labore! Assumenda quam
+            suscipit quaerat fuga!
+          </v-col>
+        </v-row>
+
+        <hr>
+        <br>
+      </div>
+
+    </v-container>
+  </v-app>
+</template>
   
-  <script>
-  import { defineComponent } from 'vue';
-  
-  // Components
-  // import HelloWorld from '../components/HelloWorld.vue';
+<script>
+import { defineComponent } from 'vue';
+
+// Components
+// import HelloWorld from '../components/HelloWorld.vue';
 //   import AppNavbar from '../components/AppNavbar.vue';
 
-  export default defineComponent({
-    name: 'AboutView',
+export default defineComponent({
+  name: 'AboutView',
+
+  setup() {
+    return {
+      slider2: 50,
+      items: [
+        { 'img': '1.png' },
+        { 'img': '1.png' },
+        { 'img': '1.png' },
+        { 'img': '1.png' },
+        { 'img': '1.png' },
+        { 'img': '1.png' },
+        { 'img': '1.png' },
+      ]
+    }
+  },
+
+  components: {
+    // AppNavbar
+  },
+});
+</script>
   
-    setup() {
-      return {
-        slider2: 50,
-        items: [
-          { 'img': '1.png' },
-          { 'img': '1.png' },
-          { 'img': '1.png' },
-          { 'img': '1.png' },
-          { 'img': '1.png' },
-          { 'img': '1.png' },
-          { 'img': '1.png' },
-        ]
-      }
-    },
-  
-    components: {
-        // AppNavbar
-    },
-  });
-  </script>
-  
-  <style scoped>
-  .v-container {
+<style scoped>.v-container {
   padding: 0 0;
 
-}
-</style>
+}</style>
   
